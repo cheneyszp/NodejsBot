87a3ec2ba343a67ac8333dc4866910307977b995
Zepeng She <shezepeng@gmail.com>
2016-12-14 20:06:07 +0800

    Update key setting

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
   
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: 'MICROSOFT_APP_ID',
+    appPassword: 'MICROSOFT_APP_PASSWORD'
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
